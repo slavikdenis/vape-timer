b7a027ca781f54504fe495360a07f1b369a108a6
Denis Slavik <denis.slavik@gmail.com>
2020-11-27 16:01:02 +0100

    Added Info icon

---
+++ src/components/icons/InfoIcon.tsx
@@ -0,0 +1,15 @@
+function InfoIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M12 10a1 1 0 00-1 1v6a1 1 0 002 0v-6a1 1 0 00-1-1zm0-4a1.25 1.25 0 101.25 1.25A1.25 1.25 0 0012 6z" />
+    </svg>
+  );
+}
+
+export default InfoIcon;
