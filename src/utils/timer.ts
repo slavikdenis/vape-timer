525bcb71f7d736797617cad7d2ade30bb58d8023
Denis Slavik <denis.slavik@gmail.com>
2020-11-27 16:12:37 +0100

    Added more timer util functions

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,3 +1,5 @@
+import { TimerPhase } from '../typings';
+
 export const addZeroIfShortNumber = (num: number) => {
   return num < 10 ? `0${num}` : num.toString();
 };
@@ -14,3 +16,29 @@ export const getWaveIndex = (time: number, waveTime: number) =>
 
 export const getWavePassedTime = (time: number, waveTime: number): number =>
   time - waveTime * getWaveIndex(time, waveTime);
+
+export const getPhaseLeftTime = (
+  time: number,
+  waveTime: number,
+  heatimeTime: number,
+): number => {
+  const waveTimePassed = getWavePassedTime(time, waveTime);
+
+  return waveTimePassed <= heatimeTime
+    ? heatimeTime - waveTimePassed
+    : waveTime - waveTimePassed;
+};
+
+export const getPhase = (
+  time: number,
+  waveTime: number,
+  heatimeTime: number,
+): TimerPhase => {
+  const waveTimePassed = getWavePassedTime(time, waveTime);
+  return waveTimePassed <= heatimeTime ? 'HEATING' : 'BLAZE';
+};
+
+export const roundNum = (num: number) => {
+  const x = Math.pow(10, 2);
+  return Math.round(num * x) / x;
+};
