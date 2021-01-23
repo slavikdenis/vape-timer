62562e556251f34dd7e6f57bcc8eb46ed93ada49
Denis Slavik <denis.slavik@gmail.com>
2021-01-23 16:17:22 +0100

    Added meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,25 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styling/theme';
 import { SettingsProvider } from '../context/settings';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider resetCSS theme={theme}>
-    <SettingsProvider>
-      <Component {...pageProps} />
-    </SettingsProvider>
-  </ChakraProvider>
+  <>
+    <Head>
+      <title>VapeTimer</title>
+      <meta charSet="utf-8" />
+
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+      />
+    </Head>
+    <ChakraProvider resetCSS theme={theme}>
+      <SettingsProvider>
+        <Component {...pageProps} />
+      </SettingsProvider>
+    </ChakraProvider>
+  </>
 );
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import { useEffect } from 'react';
 import { useColorMode } from '@chakra-ui/react';
@@ -16,18 +15,5 @@ export default function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [colorMode]);
 
-  return (
-    <>
-      <Head>
-        <title>VapeTimer</title>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-      </Head>
-
-      <DynamicTimer />
-    </>
-  );
+  return <DynamicTimer />;
 }
