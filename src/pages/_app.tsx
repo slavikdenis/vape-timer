432a62f4bf00ca9707c6113b0836d4dd6c863eb6
Denis Slavik <denis.slavik@gmail.com>
2021-05-19 22:08:48 +0200

    Fix meta charset value

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <title>VapeTimer</title>
-      <meta charSet="utf-8" />
-
+      <meta charSet="UTF-8" />
       <meta
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
