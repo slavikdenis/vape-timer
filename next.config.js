3565278e122f0c6274de73088faee0332c563f63
Denis Slavik <denis.slavik@gmail.com>
2021-05-19 21:07:17 +0200

    Enable webpack 5 for builds

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-const withPWA = require('next-pwa')
- 
+const withPWA = require('next-pwa');
+
 module.exports = withPWA({
-    pwa: {
-        disable: process.env.NODE_ENV === 'development',
-        dest: 'public'
-    }
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+  },
+  future: {
+    webpack5: true,
+  },
 });
