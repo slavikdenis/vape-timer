15995914be8b05c5165a316e75a62fffd3f86526
Denis Slavik <denis.slavik@gmail.com>
2023-06-07 19:54:22 +0200

    feat: vibrate patterns (phase change, auto stop)

--- src/__tests__/utils/hooks.test.ts
+++ src/__tests__/utils/hooks.test.ts
@@ -12,17 +12,30 @@ describe('utility hooks', () => {
     jest.clearAllMocks();
   });
 
-  it('useVibrate', () => {
+  it('useVibrate (pattern = 300)', () => {
     const { result } = renderHook(() => useVibrate());
 
     expect(result.current.isSupported).toBe(true);
     expect(typeof result.current.vibrate).toBe('function');
 
     act(() => {
-      result.current.vibrate();
+      result.current.vibrate(300);
     });
 
     expect(vibrateMock).toBeCalledWith(300);
+  });
+
+  it('useVibrate (pattern = [100, 10, 100])', () => {
+    const { result } = renderHook(() => useVibrate());
+
+    expect(result.current.isSupported).toBe(true);
+    expect(typeof result.current.vibrate).toBe('function');
+
+    act(() => {
+      result.current.vibrate([100, 10, 100]);
+    });
+
+    expect(vibrateMock).toBeCalledWith([100, 10, 100]);
   });
 
   it('useScreenWakeLock', async () => {
