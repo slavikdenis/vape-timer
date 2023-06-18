d374c513d6b98ea5bcd5d4cac0e5f427772bc801
Denis Slavik <denis.slavik@gmail.com>
2023-06-18 14:29:10 +0200

    feat: version and github link in settings

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,7 @@
+import packageJson from '../../package.json';
+
 export const isNumber = (val: unknown): val is number =>
   typeof val === 'number';
+
+// Read version from `package.json
+export const getCurrentVersion = (): string => packageJson.version;
